9fb0ac317bece3b7ff8d78dad8836a0933222e1a
Yurii Tykhovskyi <yuri.tykhovskyi@outlook.com>
2017-02-02 15:21:14 +0200

    Adding Links to Navigate

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -6,7 +6,7 @@ import {ShoppingListComponent} from "./shopping-list/shopping-list.component";
 const APP_ROUTES: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
   { path: 'recipes', component: RecipesComponent },
-  { path: 'shoping-list', component: ShoppingListComponent }
+  { path: 'shopping-list', component: ShoppingListComponent }
 ];
 
 export const routing = RouterModule.forRoot(APP_ROUTES);
