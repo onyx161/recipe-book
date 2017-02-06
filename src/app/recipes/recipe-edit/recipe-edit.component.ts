c27dc854f69fcdfa8e14f6c11db388f066f41176
Yurii Tykhovskyi <yuri.tykhovskyi@outlook.com>
2017-02-06 22:14:17 +0200

    143. Submitting the Form and Editing Recipes

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit, OnDestroy} from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 import {Subscription} from "rxjs";
 import {FormArray, FormGroup, FormControl, Validators, FormBuilder} from "@angular/forms";
 
@@ -19,7 +19,8 @@ export class RecipeEditComponent implements OnInit, OnDestroy {
 
   constructor(private route: ActivatedRoute,
               private recipeService: RecipeService,
-              private formBuilder: FormBuilder) { }
+              private formBuilder: FormBuilder,
+              private router: Router) { }
 
   ngOnInit() {
     this.subscription = this.route.params.subscribe(
@@ -37,8 +38,26 @@ export class RecipeEditComponent implements OnInit, OnDestroy {
     );
   }
 
+  onSubmit() {
+    const newRecipe = this.recipeForm.value;
+    if (this.isNew) {
+      this.recipeService.addRecipe(newRecipe);
+    } else {
+      this.recipeService.editRecipe(this.recipe, newRecipe);
+    }
+    this.navigateBack();
+  }
+
+  onCancel() {
+    this.navigateBack();
+  }
+
   ngOnDestroy() {
     this.subscription.unsubscribe();
+  }
+
+  private navigateBack() {
+    this.router.navigate(['../']);
   }
 
   private initForm() {
