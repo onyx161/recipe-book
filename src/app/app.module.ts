a289fbac80c6be020cc2e8a2b77fe412c7453418
Yurii Tykhovskyi <yuri.tykhovskyi@outlook.com>
2017-02-10 11:18:06 +0200

    190. Creating a Feature Module for Child Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,42 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { DropdownDirective } from './dropdown.directive';
 import {RecipeService} from "./recipes/recipe.service";
 import {ShoppingListService} from "./shopping-list/shopping-list.service";
 import {routing} from "./app.routing";
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-import { RecipeStartComponent } from './recipes/recipe-start.component';
 import {ShoppingListModule} from "./shopping-list/shopping-list.module";
+import {RecipesModule} from "./recipes/recipes.module";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeItemComponent,
-    RecipeDetailComponent,
-    DropdownDirective,
-    RecipeEditComponent,
-    RecipeStartComponent
+    DropdownDirective
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule,
     routing,
     HttpModule,
-    ShoppingListModule
+    ShoppingListModule,
+    RecipesModule
   ],
   providers: [
     RecipeService,
