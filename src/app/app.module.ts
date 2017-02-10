72749c0870eb1ef975619407333fd710f368dbd2
Yurii Tykhovskyi <yuri.tykhovskyi@outlook.com>
2017-02-10 11:06:36 +0200

    189. Creating a Feature Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,14 +10,13 @@ import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingListAddComponent } from './shopping-list/shopping-list-add.component';
 import { DropdownDirective } from './dropdown.directive';
 import {RecipeService} from "./recipes/recipe.service";
 import {ShoppingListService} from "./shopping-list/shopping-list.service";
 import {routing} from "./app.routing";
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeStartComponent } from './recipes/recipe-start.component';
+import {ShoppingListModule} from "./shopping-list/shopping-list.module";
 
 @NgModule({
   declarations: [
@@ -27,8 +26,6 @@ import { RecipeStartComponent } from './recipes/recipe-start.component';
     RecipeListComponent,
     RecipeItemComponent,
     RecipeDetailComponent,
-    ShoppingListComponent,
-    ShoppingListAddComponent,
     DropdownDirective,
     RecipeEditComponent,
     RecipeStartComponent
@@ -36,10 +33,10 @@ import { RecipeStartComponent } from './recipes/recipe-start.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
     ReactiveFormsModule,
     routing,
-    HttpModule
+    HttpModule,
+    ShoppingListModule
   ],
   providers: [
     RecipeService,
